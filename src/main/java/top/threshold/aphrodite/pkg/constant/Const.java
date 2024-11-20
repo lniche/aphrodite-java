c13f10649bde8e757e15338f1afbc3718a3b33d7
lcc <chengcheng.lu@windstoneservice.com>
2024-11-20 18:01:16 +0800

    update

--- src/main/java/top/threshold/aphrodite/pkg/constant/Const.java
+++ src/main/java/top/threshold/aphrodite/pkg/constant/Const.java
@@ -3,6 +3,6 @@ package top.threshold.aphrodite.pkg.constant;
 public class Const {
 
     public static final String CODE = "code";
-    public static final String TRACE_ID = "X-Request-ID";
+    public static final String TRACE_ID = "X-Request-Id";
 
 }
