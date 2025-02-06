961d12f8ac8cdf221427cd3e78157061ffe2ee07
lcc <chengcheng.lu@windstoneservice.com>
2025-02-06 09:26:23 +0800

    update

--- src/main/java/top/threshold/aphrodite/app/model/R.java
+++ src/main/java/top/threshold/aphrodite/app/model/R.java
@@ -1,34 +1,16 @@
 package top.threshold.aphrodite.app.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Setter
+@Getter
 public class R implements Serializable {
 
     private String code;
     private String message;
     private Object data;
 
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 }
